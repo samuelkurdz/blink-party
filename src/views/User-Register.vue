d29e24c2ebaeee7f73b49a4ff16a4744a4f0c48a
adebayo <kurdzsamuel@gmail.com>
2020-12-06 13:20:09 +0100

    form design commence

--- src/views/User-Register.vue
+++ src/views/User-Register.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="user-register">
     <h4 class="text-center">
-      Kindly answer the following quick survey questions,
+      Kindly answer the following quick survey questions..
     </h4>
     <div class="action-point mt-2">
       <section class="form-container">
@@ -14,17 +14,17 @@
               <option>No</option>
             </select>
           </div>
-          <div class="form-group">
+          <div class="form-group" v-if="survey.isWKUInvestor === 'Yes'">
             <label for="nameOfMoU">When did you become an investor with WKC? </label>
             <input type="date" class="form-control" v-model="survey.dateOfJoining"
                    id="nameOfMoU">
           </div>
-          <div class="form-group">
+          <div class="form-group" v-if="survey.isWKUInvestor === true">
             <label for="when">What is the name on your MoU? </label>
             <input type="email" class="form-control" v-model="survey.nameOfMoU"
                    id="when">
           </div>
-          <div class="form-group">
+          <div class="form-group" v-if="survey.isWKUInvestor === true">
             <label for="comment">Leave a comment for your experience so far </label>
             <input type="text" class="form-control" v-model="survey.comment"
                    id="comment" placeholder="enter your comment">
@@ -94,9 +94,9 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: 100%;
   width: 100vw;
-  padding: 0 1rem;
+  padding: 2rem 1rem;
 }
 
 .action-point {
@@ -107,25 +107,25 @@ export default {
 .form-container {
   background: #fff;
   position: relative;
-  padding: 20px;
+  padding: 8px 12px;
   margin: auto;
+  width: 355px;
   box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.1);
-  overflow: hidden;
 }
 
-.form-container .form {
-  /*max-width: 300px;*/
-  padding: 0 20px;
-  /*position: absolute;*/
-  top: 130px;
-  transition: transform 0.8s;
-}
+/*.form-container .form {*/
+/*  max-width: 300px;*/
+/*  padding: 0 20px;*/
+/*  position: absolute;*/
+/*  top: 130px;*/
+/*  transition: transform 0.8s;*/
+/*}*/
 
-#survey-form {
-  left: -300px;
-}
+/*#survey-form {*/
+/*  left: -300px;*/
+/*}*/
 
-#details-form {
-  left: 0;
-}
+/*#details-form {*/
+/*  left: 0;*/
+/*}*/
 </style>
