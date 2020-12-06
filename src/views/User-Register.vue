28fa5d4b4f13e200fb229a293814fb9cec80ce45
adebayo <kurdzsamuel@gmail.com>
2020-12-06 08:54:33 +0100

    form design commence

--- src/views/User-Register.vue
+++ src/views/User-Register.vue
@@ -5,7 +5,7 @@
     </h4>
     <div class="action-point mt-2">
       <section class="form-container">
-        <div class="form">
+        <div id="survey-form" class="form">
           <div class="form-group">
             <label for="isWKUInvestor">Are you a WKC Investor? </label>
             <select id="isWKUInvestor" class="form-control
@@ -28,6 +28,31 @@
             <label for="comment">Leave a comment for your experience so far </label>
             <input type="text" class="form-control" v-model="survey.comment"
                    id="comment" placeholder="enter your comment">
+          </div>
+        </div>
+        <div id="details-form" class="form">
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" v-model="survey.userName"
+                   id="name">
+          </div>
+          <div class="form-group">
+            <label for="userMail">Email</label>
+            <input type="text" class="form-control" v-model="survey.userEmail"
+                   id="userMail">
+          </div>
+          <div class="form-group">
+            <label for="location">Location</label>
+            <input type="text" class="form-control" v-model="survey.Location"
+                   id="location">
+          </div>
+          <div class="form-group">
+            <label for="gender">Gender</label>
+            <select id="gender" class="form-control
+          form-control-sm" v-model="survey.Gender">
+              <option>Male</option>
+              <option>Female</option>
+            </select>
           </div>
         </div>
         <button @click.prevent="submitSurvey"
@@ -86,5 +111,21 @@ export default {
   margin: auto;
   box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.1);
   overflow: hidden;
+}
+
+.form-container .form {
+  /*max-width: 300px;*/
+  padding: 0 20px;
+  /*position: absolute;*/
+  top: 130px;
+  transition: transform 0.8s;
+}
+
+#survey-form {
+  left: -300px;
+}
+
+#details-form {
+  left: 0;
 }
 </style>
