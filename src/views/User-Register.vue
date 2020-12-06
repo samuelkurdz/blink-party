572ef917816b587e74c320f664cb5d9da5376f32
adebayo <kurdzsamuel@gmail.com>
2020-12-06 08:26:33 +0100

    form initiated

--- src/views/User-Register.vue
+++ src/views/User-Register.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="user-register">
     <h4 class="text-center">
-      Thank you for registering for Bling Party,
-      <br>
-      Enter your email to download your ticket...
+      Kindly answer the following quick survey questions,
     </h4>
     <div class="action-point mt-2">
       <section>
         <div class="form-group">
-          <!--            <label for="Email">Email address</label>-->
-          <input type="email" class="form-control" v-model="userEmail"
-                 id="Email" placeholder="enter your email">
+          <label for="isWKUInvestor">Are you a WKC Investor? </label>
+          <select id="isWKUInvestor" class="form-control
+          form-control-sm" v-model="survey.isWKUInvestor">
+            <option>Yes</option>
+            <option>No</option>
+          </select>
         </div>
-        <button @click.prevent="emailSubmitter"
+        <div class="form-group">
+          <label for="nameOfMoU">When did you become an investor with WKC? </label>
+          <input type="date" class="form-control" v-model="survey.dateOfJoining"
+                 id="nameOfMoU">
+        </div>
+        <div class="form-group">
+          <label for="when">What is the name on your MoU? </label>
+          <input type="email" class="form-control" v-model="survey.nameOfMoU"
+                 id="when">
+        </div>
+        <div class="form-group">
+          <label for="comment">Leave a comment for your experience so far </label>
+          <input type="text" class="form-control" v-model="survey.comment"
+                 id="comment" placeholder="enter your comment">
+        </div>
+        <button @click.prevent="submitSurvey"
                 class="btn btn-sm btn-primary">
           Proceed
         </button>
@@ -25,13 +41,21 @@ export default {
   name: 'User-Register',
   data() {
     return {
-      userEmail: null,
+      survey: {
+        userName: null,
+        userEmail: null,
+        Location: null,
+        Gender: null,
+        comment: null,
+        isWKUInvestor: null,
+        dateOfJoining: null,
+        nameOfMoU: null,
+      },
     };
   },
   methods: {
-    emailSubmitter() {
-      console.log(this.userEmail);
-      this.$router.push('download');
+    submitSurvey() {
+      console.log(this.survey);
     },
   },
 };
